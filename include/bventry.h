9646de5d3f0dd4bafe35a6b2f645a37f382e452f
Rick Tillery <rtillery@ti.com>
2012-06-27 14:57:11 -0500

    Modified entry to support Linux kernel. And modified license to be compatible with Linux kernel.
    Signed-off-by: Rick Tillery <rtillery@ti.com>

--- include/bventry.h
+++ include/bventry.h
@@ -3,14 +3,13 @@
  *
  * Copyright (C) 2011 Texas Instruments, Inc.
  *
- * This file is part of BLTsville, an open application programming interface
- * (API) for accessing 2-D software or hardware implementations.
+ * This package is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
  *
- * This work is licensed under the Creative Commons Attribution-NoDerivs 3.0
- * Unported License. To view a copy of this license, visit
- * http://creativecommons.org/licenses/by-nd/3.0/ or send a letter to
- * Creative Commons, 444 Castro Street, Suite 900, Mountain View, California,
- * 94041, USA.
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  */
 
 #ifndef BVENTRY_H
@@ -22,11 +21,20 @@ struct bvbltparams;
 struct bvcopparams;
 /*
  * bv_*() - These are the API calls for BLTsville.  The client needs to
- * import these from the shared library.
+ * import these from the kernel module.  Normally this will be via a call
+ * to a function like bv*_init(&bventry).
  */
 typedef enum bverror (*BVFN_MAP)(struct bvbuffdesc *buffdesc);
 typedef enum bverror (*BVFN_BLT)(struct bvbltparams *bltparms);
 typedef enum bverror (*BVFN_UNMAP)(struct bvbuffdesc *buffdesc);
 typedef enum bverror (*BVFN_CACHE)(struct bvcopparams *copparams);
+
+struct bventry {
+	unsigned int structsize;	/* used to identify struct version */
+	BVFN_MAP bv_map;
+	BVFN_UNMAP bv_unmap;
+	BVFN_BLT bv_blt;
+	BVFN_CACHE bv_cache;
+};
 
 #endif /* BVENTRY_H */
